0ff7137bd4b59cec4f3b664c016260f6c4df8294
wuzhiming <wuzhiming_cd@keruyun.com>
2017-09-30 13:55:34 +0800

    init

---
+++ test/functional_tests.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/28 16:33
+# @Author  : Aries
+# @Site    : 
+# @File    : functional_tests.py
+# @Software: PyCharm
+import unittest,time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class NewVisitorTest(unittest.TestCase):  # ➊
+
+
+    def setUp(self):  # ➋
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):  # ➌
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):  # ➍
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+        # 她注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title)  # ➎
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # 她在一个文本框中输入了“Buy peacock feathers（购买孔雀羽毛） ”
+        # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
+        # 她按回车键后，页面更新了
+        # 待办事项表格中显示了“1: Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(10)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
+        # 页面中又显示了一个文本框，可以输入其他的待办事项
+        # 她输入了“Use peacock feathers to make a fly（使用孔雀羽毛做假蝇） ”
+        # 伊迪丝做事很有条理
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':  # ➐
+    unittest.main(warnings='ignore')  # ➑
